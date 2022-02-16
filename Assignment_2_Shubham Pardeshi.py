f23de519f0320b474194a838300f2c014466ed22
pardeshi09 <64329254+pardeshi09@users.noreply.github.com>
2022-02-16 13:35:14 +0530

    Add files via upload

---
+++ Assignment_2_Shubham Pardeshi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+1.What are the two values of the Boolean data type? How do you write them?
+    0 and 1 are the two values of the Boolean data type.
+    For 0 we have False and for True we have 1.
+    
+2. What are the three different types of Boolean operators?
+
+Bollean operations are simple arithmetic of True and False values.These values can be manipulated by the use of Boolean operators which include AND,OR,and NOT.
+
